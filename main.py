67a2e058b346960b81daa615c34178c35a198583
leekd97 <leekd97@naver.com>
2023-04-18 18:44:00 +0900

    second commit

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, send_file
 import mimetypes, os, uuid, time
 from PIL import Image
 import PIL.ImageOps
+from roboflow import Roboflow
+rf = Roboflow(api_key="8dasIsb8Hg2wSLkqOHd4")
+project = rf.workspace("smoking-wchnj").project("senior_project-3ohay")
+model = project.version(7).model
 
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
@@ -9,34 +13,18 @@ app = Flask(__name__)
 
 
 def model_process(filename):
-    # TODO: Change it to make YOLO output
-    time.sleep(3)
     image = Image.open("static/images/{}".format(filename))
     output_img = format(uuid.uuid4()) + ".png"
-    if image.mode == 'RGBA':
-        r, g, b, a = image.split()
-        rgb_image = Image.merge('RGB', (r, g, b))
-        inverted_image = PIL.ImageOps.invert(rgb_image)
-        r2, g2, b2 = inverted_image.split()
-        final_transparent_image = Image.merge('RGBA', (r2, g2, b2, a))
-        final_transparent_image.save('static/images/{}'.format(output_img))
 
-    else:
-        inverted_image = PIL.ImageOps.invert(image)
-        inverted_image.save('static/images/{}'.format(output_img))
+    model_output = model.predict("static/images/{}".format(filename), confidence=40, overlap=30)
+    model_output.save('static/images/{}'.format(output_img))
 
     return_obj = {
         "img_name" : "output_img/{}".format(output_img),
 
-        "log_info" : {
-            "a" : "BB",
-            "b" : 123
-        }
+        "log_info" : model_output.json()['predictions']
     }
     return return_obj
-
-
-
 
 @app.route('/')
 def render_html():
